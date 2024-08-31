da9d236b77ee588baab80f0848d63406d0df5d12
Mahmoud Hamdi <ma7moudselim01@gmail.com>
2024-09-01 00:49:40 +0300

    Assignment03MVC

---
+++ Context/CompanyDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Company.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Context
+{
+	public class CompanyDBContext : DbContext
+	{
+		public CompanyDBContext()
+		{
+		}
+
+		public CompanyDBContext(DbContextOptions<CompanyDBContext> options) : base(options)
+		{
+		}
+		
+		public DbSet<Employee> employees { get; set; }	
+		public DbSet<Department> departments { get; set; }	
+
+		
+	}
+}
